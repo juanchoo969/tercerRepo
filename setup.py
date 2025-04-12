5f9a8f34e81397d76c445b8037a4292f8521997d
juanchoo969 <dhortua959@gmail.com>
2025-04-12 15:08:18 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                                   # Nombre del paquete
+    version="0.1.0",                                     # Versión inicial
+    packages=find_packages(),                            # Paquetes a incluir
+    description="Un paquete pip simple de saludo",       # Breve descripción
+    author="Juan Hortua ",                               # Tu nombre
+    author_email="dhortua959@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/juanchoo969/tercerrepo",          # URL del proyecto
+)
